b76e3f9212def33fd656ebd9f373ab0ab461f3cb
omar7amed <omar7amed411@gmail.com>
2024-09-04 12:26:15 +0300

    assigments 1 example 1

---
+++ ex 1/main.c
@@ -0,0 +1,97 @@
+//omar mohamed mohamed assigment
+
+
+//example 1
+//
+//#include "stdio.h"
+//
+//int main ()
+//{
+//	printf("\n C Programing \n");
+//
+//}
+
+//example 2
+
+//#include "stdio.h"
+//
+//int main ()
+//{
+//	int x;
+//	printf("\nEnter a Integer: ");
+//	fflush(stdout);
+//	scanf ("%d",&x);
+//	printf("\nYou entered : %d",x);
+//
+//
+//}
+
+//example 3
+
+//#include "stdio.h"
+//
+//int main ()
+//{
+//	int sum;
+//	int x,y;
+//	printf("\nEnter two Integers: ");
+//	fflush(stdout);
+//	scanf ("%d %d",&x,&y);
+//	fflush(stdin);
+//
+//	sum=x+y;
+//	printf("\nsum :" "%d",sum);
+//}
+
+//example 4
+
+//#include "stdio.h"
+//
+//int main ()
+//{
+//	float product;
+//	float x,y;
+//	printf("\nEnter two Integers: ");
+//	fflush(stdout);
+//	scanf ("%f %f",&x,&y);
+//	fflush(stdin);
+//	product=x*y;
+//	printf("\nproduct : %f",product);
+//}
+
+//example 5
+//#include "stdio.h"
+//
+//int main ()
+//{
+//	char x;
+//	printf("Enter a character : \n");
+//	fflush(stdout);
+//	scanf("%c",&x);
+//	printf("ASCII value of %c :  %d",x,x);
+//}
+
+
+//example 6
+
+//#include "stdio.h"
+//
+//int main ()
+//{
+//
+//	float x,y,temp;
+//
+//	printf("enter value of numb 1\n ");
+//	fflush(stdout);
+//	scanf("%f",&x);
+//	printf("enter value of numb 2\n");
+//	fflush(stdout);
+//	scanf("%f",&y);
+//
+//	temp=x;
+//	x=y;
+//	y=temp;
+//
+//	printf("after swaping numb 1 = %f\n",x);
+//	printf("after swaping numb 2 =%f ",y);
+//}
